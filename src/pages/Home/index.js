845209c9f14cacacd6934421102d09b7d091f132
unknown <gustavoaraujo600@hotmail.com>
2020-04-16 13:36:47 -0300

    front-end - OK

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,29 +1,57 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
+import Emoji from '../../components/Emoji';
 import './styles.css';
 
 export default function Home() {
+  const history = useHistory();
+
   return (
     <div className="home-container">
-      <h1>Caixa de Sugestão da Turma de SI</h1>
-      <button>contribuir</button>
+      <h1>Caixa de sugestão da turma de SI.</h1>
+      <button onClick={() => { history.push('/Cadastro') }}>contribuir</button>
 
       <ul>
         <li>
           <h2>Gustavo Araujo</h2>
-          <p>feedback... feedback... feedback... feedback... feedback...</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          <p className="date">01/04/2020</p>
         </li>
 
         <li>
-          <h2>Gustavo Araujo</h2>
-          <p>feedback... feedback... feedback... feedback... feedback...</p>
+          <h2>Laura Alves</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          <p className="date">01/04/2020</p>
         </li>
 
         <li>
-          <h2>Gustavo Araujo</h2>
-          <p>feedback... feedback... feedback... feedback... feedback...</p>
+          <h2>Matheus Carvalho</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          <p className="date">01/04/2020</p>
+        </li>
+        <li>
+          <h2>Iago Silva</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          <p className="date">01/04/2020</p>
+        </li>
+
+        <li>
+          <h2>Erick Ribeiro</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          <p className="date">01/04/2020</p>
+        </li>
+
+        <li>
+          <h2>Alef Henrique</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          <p className="date">01/04/2020</p>
         </li>
       </ul>
+
+      <footer>
+        <h4>&copy; 2020 - Desenvolvido por alunos de SI do Pitágoras <Emoji symbol='🚀' label='Emoji foguete' /></h4>
+      </footer>
 
     </div>
   );
--- src/pages/RegisterSuggestion/index.js
+++ src/pages/RegisterSuggestion/index.js
@@ -1,24 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
 import logoImg from '../../assets/logo.png';
 import './styles.css';
-
 import Emoji from '../../components/Emoji';
 
 export default function RegisterSuggestion() {
+  const [name, setName] = useState('');
+  const [suggestion, setSuggestion] = useState('');
+  const [phone, setPhone] = useState('');
+
+  const history = useHistory();
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    console.log(name, suggestion, phone);
+
+  }
+
   return (
     <div className="register-container">
       <img src={logoImg} alt="logo pitágoras" />
       <h4>Caixa de sugestão para alunos do Pitágoras</h4>
 
-      <form>
+      <form onSubmit={handleSubmit}>
         <label for="nome">Nome:*</label><br />
-        <input type="text" name="nome" required /><br />
+        <input
+          type="text"
+          name="nome"
+          required
+          value={name}
+          onChange={e => setName(e.target.value)}
+        /><br />
         <label for="sugestao">Sugestao:*</label><br />
-        <textarea cols="30" rows="5" required></textarea><br />
+        <textarea
+          cols="30"
+          rows="5"
+          required
+          value={suggestion}
+          onChange={e => setSuggestion(e.target.value)}
+        ></textarea><br />
         <label for="telefone">Telefone: <span>(opcional)</span></label><br />
-        <input type="tel" name="telefone" placeholder="DDD + Numero" /><br />
+        <input
+          type="tel"
+          name="telefone"
+          placeholder="DDD + Numero"
+          value={phone}
+          onChange={e => setPhone(e.target.value)}
+        /><br />
         <button type="submit" class="button">Cadastrar</button>
+        <button class="button" onClick={() => { history.push('/') }}>Voltar</button>
       </form>
 
       <footer>

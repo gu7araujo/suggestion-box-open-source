d85e91a8faacb842943565cfbb72f06021ed2640
unknown <gustavoaraujo600@hotmail.com>
2020-04-20 11:41:01 -0300

    add api verifications

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,11 +1,25 @@
 const express = require('express');
+const { celebrate, Segments, Joi } = require('celebrate');
 
 const SuggestionController = require('./controllers/Suggestion_Controller');
 
 const routes = express.Router();
 
 routes.get('/suggestions', SuggestionController.index);
-routes.post('/suggestions', SuggestionController.create);
-routes.delete('/suggestions/:id', SuggestionController.delete);
+
+routes.post('/suggestions', celebrate({
+  [Segments.BODY]: Joi.object().keys({
+    name: Joi.string().required().min(3),
+    suggestion: Joi.string().required().min(20),
+    phone: Joi.string(),
+    date: Joi.string().required()
+  })
+}), SuggestionController.create);
+
+routes.delete('/suggestions/:id', celebrate({
+  [Segments.PARAMS]: Joi.object().keys({
+    id: Joi.number().required()
+  })
+}), SuggestionController.delete);
 
 module.exports = routes;

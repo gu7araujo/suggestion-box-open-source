67e63c5d490cf47c62c4dc5d2f5ed51b9339c296
unknown <gustavoaraujo600@hotmail.com>
2020-04-28 19:32:28 -0300

    loading on the home screen

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 
+import ReactLoading from 'react-loading';
 import Emoji from '../../components/Emoji';
 import './styles.css';
 
@@ -8,10 +9,12 @@ import api from '../../services/api';
 
 export default function Home() {
   const [suggestions, setSuggestions] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     api.get('suggestions').then(response => {
       setSuggestions(response.data);
+      setLoading(false);
     })
   }, [])
 
@@ -22,15 +25,17 @@ export default function Home() {
       <h1>Caixa de sugestão da turma de SI.</h1>
       <button onClick={() => { history.push('/Cadastro') }}>contribuir</button>
 
-      <ul>
-        {suggestions.map(suggestion => (
-          <li key={suggestion.suggestion_id}>
-            <h2>{suggestion.name}</h2>
-            <p>{suggestion.suggestion}</p>
-            <p className="date">{suggestion.date}</p>
-          </li>
-        )).reverse()}
-      </ul>
+      {loading ? <ReactLoading type={"spinningBubbles"} color={"black"} /> :
+        <ul>
+          {suggestions.map(suggestion => (
+            <li key={suggestion.suggestion_id}>
+              <h2>{suggestion.name}</h2>
+              <p>{suggestion.suggestion}</p>
+              <p className="date">{suggestion.date}</p>
+            </li>
+          )).reverse()}
+        </ul>
+      }
 
       <footer>
         <h4>&copy; 2020 - Desenvolvido por alunos de SI do Pitágoras <Emoji symbol='🚀' label='Emoji foguete' /></h4>
